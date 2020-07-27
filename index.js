5cd72749800102cdab07089a123a21e84641e979
willos2006 <30528000+willos2006@users.noreply.github.com>
2020-07-27 14:41:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-//const TOKEN = process.env.TOKEN;
+const TOKEN = process.env.TOKEN;
 
 bot.login(TOKEN);
 
