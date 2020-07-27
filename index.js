1f94943fa0c682786ffc2b96f2f7edc4a747dda4
Will O'Shaughnessy <willisthebest2006@gmail.com>
2020-07-27 19:23:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -116,17 +116,16 @@ msg.channel.send("You can only use me in the truth or dare channel!"); }
 	msg.channel.send("Hey! I'm the truth or dare bot here on Nerve! With me, you don't need to worry about thinking of truths or dares! Just use `-truth` and `-dare` when it is your turn and the bot will think of one for you! Of course, my creator is only human so please feel free to DM <@303097521314725890> with more truth or dares!");
 	}
 
-async function changeStat(){
-    await sleep(2000);
-    bot.user.setActivity('In development', {
-        type: 'LISTENING'
-    });
-    await sleep(2000);
-    bot.user.setActivity('Truth or Dare', {
-        type: 'PLAYING'
-    });
-    changeStat();
+var index = 0;
+setInterval({
+    if(index == 0){
+    bot.setActivity("my development", {type: "WATCHING"});
+    index = 1;
 }
+else{
+    index = 0;
+     bot.setActivity("Truth or Dare", {type: "PLAYING"});
+}, 2000);
 
 changeStat();
 
