9d9a5f3e74b3fb55beaee3edf88dd9970a271883
Will O'Shaughnessy <willisthebest2006@gmail.com>
2020-07-29 18:46:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -190,4 +190,5 @@ if(msg.channel.id === '738077497761202246'){
             msg.channel.send("<@" + message.author.id + ">  has ruined it! The current number has been reset... start again from 1!");
         }
     }
+}
 });
